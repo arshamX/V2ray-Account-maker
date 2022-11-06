37ecded3a09e4c2dbe211d3785f98278d9329d17
Arsham Forootan <arshamforootan@proton.me>
2022-11-06 12:09:44 +0330

    adding constants namespace and Information struct and fields

---
+++ Constants/Information.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    public struct Information
+    {
+        public static readonly string[] IP = { "http://IP:54321", "IP" };
+        public static readonly string PORT = "54321";
+        public static readonly string DOMAIN = "DOMAIN";
+        public static readonly string CHARS = "ef14adc2509b3867";
+        public static readonly string LICENSE = "IRONBARCODE-BOARD4ALL.BIZ-771231-DD5FA6-14735D713C-3EB559D2-NEx-7D4";
+        public static readonly string[] COOKIE = { "session","Cookie" };
+    }
+}
